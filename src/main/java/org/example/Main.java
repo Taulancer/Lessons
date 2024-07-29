28b2ce4ad86bbba21722bbc6d0ade11de702748c
Taulancer <74763408+Taulancer@users.noreply.github.com>
2024-07-29 22:05:46 +0300

    Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,148 +1,19 @@
 package org.example;
 
-
 public class Main {
     public static void main(String[] args) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
-        System.out.println(isSumInRange(5, 10));
-        printNumberSign(0);
-        System.out.println(isNegative(-5));
-        repeatString("Hello", 3);
-        System.out.println(isLeapYear(2024));
-        flipArrayElements();
-        fillArray();
-        processArray();
-        printDiagonalMatrix(5); // Задание 13
-        int[] newArray = createArray(5, 10); // Задание 14
-        for (int value : newArray) {
-            System.out.print(value + " ");
-        }
-        System.out.println();
-    }
+        Employee[] employees = new Employee[5];
 
-    public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
+        employees[0] = new Employee("Ivanov Ivan", "Engineer", "ivanov@mailbox.com", "84615464", 70000, 30);
+        employees[1] = new Employee("Petrov Petr", "Manager", "petrov@mailbox.com", "892312313", 50000, 35);
+        employees[2] = new Employee("Sidorov Sidr", "Director", "sidorov@mailbox.com", "892312314", 100000, 40);
+        employees[3] = new Employee("Smirnov Smirnyaga", "Developer", "smirnov@mailbox.com", "892312315", 80000, 25);
+        employees[4] = new Employee("Kuznetsov Kuzya", "Designer", "kuznetsov@mailbox.com", "892312316", 60000, 28);
 
-    public static void checkSumSign() {
-        int a = 5;
-        int b = -10;
-        if ((a + b) >= 0) {
-            System.out.println("Сумма положительная");
-        } else {
-            System.out.println("Сумма отрицательная");
-        }
-    }
-
-    public static void printColor() {
-        int value = 50;
-        if (value <= 0) {
-            System.out.println("Красный");
-        } else if (value > 0 && value <= 100) {
-            System.out.println("Желтый");
-        } else {
-            System.out.println("Зеленый");
-        }
-    }
-
-    public static void compareNumbers() {
-        int a = 7;
-        int b = 5;
-        if (a >= b) {
-            System.out.println("a >= b");
-        } else {
-            System.out.println("a < b");
-        }
-    }
-
-    public static boolean isSumInRange(int a, int b) {
-
-        return (a + b) >= 10 && (a + b) <= 20;
-    }
-
-    public static void printNumberSign(int num) {
-        if (num >= 0) {
-            System.out.println("Число положительное");
-        } else {
-            System.out.println("Число отрицательное");
-        }
-    }
-
-    public static boolean isNegative(int num) {
-
-        return num < 0;
-    }
-
-    public static void repeatString(String str, int times) {
-        for (int i = 0; i < times; i++) {
-            System.out.println(str);
-        }
-    }
-
-    public static boolean isLeapYear(int year) {
-        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-    }
-
-    public static void flipArrayElements() {
-        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = arr[i] == 0 ? 1 : 0;
-        }
-        for (int value : arr) {
-            System.out.print(value + " ");
-        }
-        System.out.println();
-    }
-
-    public static void fillArray() {
-        int[] arr = new int[100];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i + 1;
-        }
-        for (int value : arr) {
-            System.out.print(value + " ");
-        }
-        System.out.println();
-    }
-
-    public static void processArray() {
-        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < 6) {
-                arr[i] *= 2;
-            }
-        }
-        for (int value : arr) {
-            System.out.print(value + " ");
-        }
-        System.out.println();
-    }
-
-    public static void printDiagonalMatrix(int size) {
-        int[][] matrix = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            matrix[i][i] = 1; // Заполнение главной диагонали
-            matrix[i][size - i - 1] = 1; // Заполнение побочной диагонали
-        }
-        for (int[] row : matrix) {
-            for (int value : row) {
-                System.out.print(value + " ");
-            }
+        for (Employee employee : employees) {
+            employee.printInfo();
             System.out.println();
         }
-    }
-
-    public static int[] createArray(int len, int initialValue) {
-        int[] array = new int[len];
-        for (int i = 0; i < len; i++) {
-            array[i] = initialValue;
-        }
-        return array;
     }
 }
 
